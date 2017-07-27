2afab3b358ccff0a302148525c109ba7459d444d
Selva <selvamuthukumar.thiruarul@disney.com>
2017-07-27 15:28:55 -0400

    Fix travis

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from setuptools import setup
 
 setup(name='visualize_thirukural', version='1.0.0',
-	  url='https://github.com/selvathiruarul/twitter_thirukural', author='selva')
+      url='https://github.com/selvathiruarul/twitter_thirukural', author='selva')
